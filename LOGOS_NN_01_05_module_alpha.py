380565bea8a4946b9f3081a42d056339c6be0cad
Gunjan R. Ramteke <gunjanrramteke@gmail.com>
2025-08-30 14:51:16 +0530

    Add files via upload

---
+++ LOGOS_NN_01_05_module_alpha.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+import copy
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+
+
+class MLP_model(nn.Module):
+
+    def __init__(sf, in_channel, out_channel):
+        super().__init__()
+
+        sf.layers = nn.Sequential(
+        nn.Linear(in_channel, 256),
+        nn.ReLU(),
+        nn.BatchNorm1d(256),
+        nn.Linear(256, 200),
+        nn.ReLU(),
+        nn.Linear(200, 160),
+        nn.ReLU(),
+        nn.Linear(160, 128),
+        nn.ReLU(),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Linear(64, 32),
+        nn.ReLU(),
+        nn.Linear(32, 16),
+        nn.ReLU(),
+        nn.Linear(16, out_channel))
+
+    def forward(sf,x):
+        return(sf.layers(x))
+
+
+class NeuralNetwork_MAIN():
+
+    def __init__(sf):
+
+        pass
+
+    def NeuralNetwork(sf, I_grp, prop, filename):
+
+        # Splitting the Data Into Training And Testing Set
+
+        X_train, X_test, y_train, y_test = train_test_split(I_grp, prop, train_size=0.7, shuffle=True)
+
+        # Standerdization Of Data
+
+        sf.scaler = StandardScaler()
+        sf.scaler.fit(X_train)
+        X_train = sf.scaler.transform(X_train)
+        X_test  = sf.scaler.transform(X_test)
+
+        # Converting The Dataset
+
+        X_train = torch.tensor(X_train, dtype=torch.float32)
+        y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
+        X_test = torch.tensor(X_test, dtype=torch.float32)
+        y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
+        # Initialize NN Parameter
+
+        learning_rate = 0.001
+        n_epochs = 300
+        batch_size = 150
+
+
+        # Initiate Training
+
+        sf.model = MLP_model(len(X_train[0]), len(y_train[0]))
+        optimizer = optim.Adam(sf.model.parameters(), learning_rate)
+        loss_fn = nn.MSELoss()
+       
+        batch_start = torch.arange(0, len(X_train), batch_size)
+        best_mse = np.inf
+        best_weights = None
+        history = []
+
+        '''   Training Loop  '''
+
+        for epoch in range(n_epochs):
+            sf.model.train()
+
+            with tqdm.tqdm(batch_start, unit="batch", mininterval=0, disable=True) as bar:
+                bar.set_description(f"Epoch {epoch}")
+                for start in bar:
+
+                    # A Batch
+                    X_batch = X_train[start:start+batch_size]
+                    y_batch = y_train[start:start+batch_size]
+
+                    # Forward Pass
+                    y_pred = sf.model(X_batch)
+                    loss = loss_fn(y_pred, y_batch)
+
+                    # Backward Pass
+                    optimizer.zero_grad()
+                    loss.backward()
+
+                    # Updating Weights
+                    optimizer.step()
+
+                    # Printing Progress
+                    bar.set_postfix(mse=float(loss))
+
+            # Acuracy Evaluation at The end of each epoch
+
+            sf.model.eval()
+            y_pred = sf.model(X_test)
+            mse = loss_fn(y_pred, y_test)
+            mse = float(mse)
+            history.append(mse)
+
+            if mse < best_mse:
+                best_mse = mse
+                best_weights = copy.deepcopy(sf.model.state_dict())
+
+        # Restore Model And Return Accuracy
+
+        torch.save(sf.model.state_dict(), filename)
+        print("MSE: %.2f" % best_mse)
+        print("RMSE: %.2f" % np.sqrt(best_mse))
+        plt.plot(history)
+        plt.close()
+
+    def Predict(sf, X_dat):
+
+        X_dat = sf.scaler.transform(X_dat)
+        X_dat = torch.tensor(X_dat, dtype=torch.float32)
+        y_pred = sf.model(X_dat)
+        y_pred = y_pred.detach().numpy()
+
+        return(y_pred)
+
+
+if __name__ == "__main__":
+
+
+    X_fl = open('LOGOS-NN-Input-Training-Set.csv')
+    X_fl = X_fl.readlines()
+    XinpF = [ [] for i in range(len(X_fl))]
+
+    for a,i in enumerate(X_fl):
+        xalj = i.split()
+        XinpF[a] = [float(xalj[j]) for j in range(0,len(xalj) ,2)]
+
+    Yprop = open('LOGOS-NN-Prop_06_ag3_Training-Set.csv')
+    Yprop = Yprop.read()
+    Yprop = Yprop.split()
+    Yprop = np.array([float(i) for i in Yprop])
+    XinpF = np.array(XinpF)
+
+    PercentSplit = 0.7
+    filename = 'BestWeights_06_gamma.pth.tar'
+    X_train, X_test, Y_train, Y_test = train_test_split(XinpF, Yprop, train_size=PercentSplit, shuffle=True)
+
+    # Training
+
+    NNetwork = NeuralNetwork_MAIN()
+    NNetwork.NeuralNetwork(X_train, Y_train, filename)
+
+    # Testing_1
+
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    Y_test = torch.tensor(Y_test, dtype=torch.float32).reshape(-1, 1)
+    Out_pred = NNetwork.Predict(X_test)
+
+    print('Weights are Stored in : BestWeights_06_gamma.pth.tar')
